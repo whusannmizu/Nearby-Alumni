623e2174787adb7c7abe0804af1c88c88279c9d7
马润 <13098830176@163.com>
2019-09-02 21:00:36 +0800

    我的和登陆界面

---
+++ app/src/main/java/com/sannmizu/nearby_alumni/utils/encoder/CEFormatException.java
@@ -0,0 +1,11 @@
+package com.sannmizu.nearby_alumni.utils.encoder;
+
+import java.io.IOException;
+
+public class CEFormatException extends IOException
+{
+	public CEFormatException(String s)
+	{
+		super(s);
+	}
+}
---
+++ app/src/main/java/com/sannmizu/nearby_alumni/utils/encoder/CEStreamExhausted.java
@@ -0,0 +1,8 @@
+package com.sannmizu.nearby_alumni.utils.encoder;
+
+import java.io.IOException;
+
+public class CEStreamExhausted extends IOException
+{
+
+}
